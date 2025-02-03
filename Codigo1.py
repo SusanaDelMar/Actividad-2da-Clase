f739ee44be271c53dbc9d2af9edc05e3cea0117b
Susana del Mar <susanagualy@gmail.com>
2025-02-03 12:50:55 -0500

    Primer Cambio

---
+++ Codigo1.py
@@ -0,0 +1,51 @@
+"""
+#EJERCICIO 1
+print("Hola Mundo")
+num1=52
+num2=8
+#aca se asignan los valores a dos variables
+nombre="Susana del Mar"
+print("Hola Susana del Mar")
+print("Hola"+nombre)
+print("num2")
+print(num2)
+suma=num1+num2
+print(suma)
+nombre="Susana del Mar"
+
+
+#EJERCICIO 2
+
+nombre=input("¿Cómo te llamas ? ")
+print("Hola " +nombre)
+num1="5"
+num2="6"
+suma=num1+num2
+print(suma)
+
+
+#EJERCICIO 3
+
+nombre=input("¿Cómo te llamas? ")
+print("Hola " +nombre)
+num1=int(input("ingrese el primer numer "))
+num2=int(input("Ingrese el segundonumer "))
+suma=num1+num2
+print("El resultado de la suma es", suma)
+resta=num1-num2
+print("El resultado de la resta es", resta)
+Multiplicacion=num1*num2
+print("El resultado de la Multiplicacion es", Multiplicacion)
+division=num1-num2
+print("El resultado de la división es", division)
+
+
+#EJERCICIO 4
+nombre=input("¿Cómo te llamas? ")
+print("Hola " +nombre)
+num1=int(input("ingrese el día de nacimiento "))
+num2=int(input("Ingrese el mes de nacimiento "))
+num3=int(input("ingrese el año de nacimiento "))
+print("La fecha de nacimiento es "+str(num1)+"/"+str(num2)+"/"+str(num3))
+"""
+
